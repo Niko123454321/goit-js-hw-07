a9d05d5eaac3b0bc9610ebb4c4369568df69f39a
Niko123454321 <kvbsnsk@gmail.com>
2022-11-14 13:36:26 +0200

    markup, modalLinkKClick

---
+++ js/01-gallery.js
@@ -0,0 +1,40 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery')
+const galleryMarkup=createGalleryItemsMarkup(galleryItems)
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems.map(({preview,original,description}) => {
+    return `
+ <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+  }).join('');
+  
+console.log(markup[0])
+
+}
+
+galleryContainer.addEventListener('click', onGalleryItemsClick);
+
+
+function onGalleryItemsClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return
+  }
+  
+  console.log(event.target.dataset.source);
+}
+
+
